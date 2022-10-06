3fc58a1a2dee4d91df9d9ecdfa709e2f31f9b43e
saikrishnaegcc2 <90199989+saikrishnaegcc2@users.noreply.github.com>
2022-10-06 21:36:22 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -60,7 +60,7 @@ async def bin(_, m: Message):
             inputm = m.text.split(None, 1)[1]
             bincode = 6
             ask = inputm[:bincode]
-            req = requests.get(f"https://bins-su-api.vercel.app/api/{ask}").json()
+            req = requests.get(f"https://dococbin-api.vercel.app/bin={ask}").json()
             res = req["result"]
 
             if res == False:
